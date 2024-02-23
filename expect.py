28372e928c6778b3aafe5ce15a06b4cd59b6d813
erpreciso <trepreciso@gmail.com>
2024-02-23 10:58:42 +0100

    Documentation.

--- expect.py
+++ expect.py
@@ -16,6 +16,12 @@ multiplier = {
 
 
 def _is_origin(entry):
+    """Return if entry is an origin for an expectation.
+
+    :param entry: Entry
+    :returns: Boolean
+
+    """
     assert isinstance(entry, data.Transaction)
     v = True
     v = v and entry.meta.get('expected')
@@ -24,11 +30,34 @@ def _is_origin(entry):
 
 
 def tag_entry(tag: str, entry):
+    """Add tag to an entry.
+
+    :param tag: Tag to be added
+    :param entry: Entry
+    :returns: 
+
+    """
     new_tagset = entry.tags.union(set([tag]))
     return entry._replace(tags=new_tagset)
 
 
 def get_expected_dates(entry):
+    """Get expected dates based on entry parameters.
+
+    :param entry: Entry
+    :returns: list of expected dates
+
+    Reads entry metadata:
+    - frequency :: must be in the RFC 5545 names
+      (https://datatracker.ietf.org/doc/html/rfc5545),
+      otherwise default to monthly
+    - interval :: interval between expectations
+    - until OR count OR duration_in_years :: either end date, or
+      number of expected occurrences. The two meta MUST NOT be given
+      both in the same entry
+      (https://dateutil.readthedocs.io/en/stable/rrule.html#dateutil.rrule.rrule)
+
+    """
     freq_param = entry.meta.get('frequency', 'monthly').upper()
     freq = FREQNAMES.index(freq_param)
     interval = entry.meta.get('interval', 1)
@@ -49,12 +78,25 @@ def get_expected_dates(entry):
 
 
 def all_equal(iterable):
-    # https://stackoverflow.com/a/3844832/2265140
+    """Return if all members of iterable are equal.
+    
+    :param iterable: An iterable
+    :returns: Boolean
+
+    https://stackoverflow.com/a/3844832/2265140
+    """
     g = groupby(iterable)
     return next(g, True) and not next(g, False)
 
 
 def _have_same_postings_accounts(entries: list) -> bool:
+    """Return if all entries provided have the same postings accounts.
+
+    :param entries: List of entries
+    :returns: Boolean
+
+    """
+    
     postings = []
     for e in entries:
         postings.append(set([p.account for p in e.postings]))
@@ -65,17 +107,38 @@ def _have_same_postings_accounts(entries: list) -> bool:
 
 def _is_overtrown_by_real_entry(exp_entry,
                                 entries,
-                                margin: dt.timedelta = dt.timedelta(days=7)):
+                                margin: int = 7):
+    """Return if entry is overtrown by any existing entry.
+
+    :param exp_entry: Entry to be checked
+    :param entries: Existing entries
+    :param margin: Margin in days
+    :returns: Boolean
+
+    Return True if any entry is either future, or older but within a
+    margin in days. This to exclude expected dates obsolete because
+    actually duplicates of existing entries either occurred in the future
+    or in the immediate past.
+
+    """
     for real_entry in entries:
         if _have_same_postings_accounts([exp_entry, real_entry]):
-            if real_entry.date >= exp_entry.date - margin:
+            margin_days = dt.timedelta(days=margin)
+            if real_entry.date >= exp_entry.date - margin_days:
                 return True
     return False
     # pdb.set_trace()
 
 
 def expect(entries, options_map, config_string="{}"):
-    ""
+    """Create expected entries based on entry metadata.
+
+    :param entries: Entries
+    :param options_map: Unused option map
+    :param config_string: Unused configuration string
+    :returns: Existing entries and expected entries, and errors.
+
+    """
     errors = []
     forecasted = []
     # process, from now on, only data.Transactions
