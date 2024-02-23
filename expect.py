a93be05c4c37694e267047fdf5f8eee709c0eca6
erpreciso <trepreciso@gmail.com>
2024-02-23 10:42:13 +0100

    Working plugin. More tests.

--- expect.py
+++ expect.py
@@ -1,8 +1,8 @@
-import collections
 import pdb
 from beancount.core import data
 from dateutil.rrule import rrule, FREQNAMES
-from datetime import date
+import datetime as dt
+from itertools import groupby
 
 __plugins__ = ['expect']
 
@@ -16,7 +16,8 @@ multiplier = {
 
 
 def _is_origin(entry):
-    v = isinstance(entry, data.Transaction)
+    assert isinstance(entry, data.Transaction)
+    v = True
     v = v and entry.meta.get('expected')
     v = v and entry.meta.get('expected').lower() in ["true", "t"]
     return v
@@ -35,27 +36,66 @@ def get_expected_dates(entry):
     _count = multiplier.get(freq_param) * duration / interval
     count = entry.meta.get('count', _count)
     if entry.meta.get('until'):
-        until = date.fromisoformat(entry.meta.get('until'))
+        until = dt.date.fromisoformat(entry.meta.get('until'))
     else:
         until = None
-    expected_dates = [dt.date() for dt in rrule(freq=freq,
-                                                dtstart=entry.date,
-                                                count=count,
-                                                interval=interval,
-                                                until=until)]
+    expected_dates = [d.date() for d in rrule(freq=freq,
+                                              dtstart=entry.date,
+                                              count=count,
+                                              interval=interval,
+                                              until=until)]
+    # TODO ensure recurrences are on business days
     return expected_dates
+
+
+def all_equal(iterable):
+    # https://stackoverflow.com/a/3844832/2265140
+    g = groupby(iterable)
+    return next(g, True) and not next(g, False)
+
+
+def _have_same_postings_accounts(entries: list) -> bool:
+    postings = []
+    for e in entries:
+        postings.append(set([p.account for p in e.postings]))
+    if all_equal(postings):
+        return True
+    return False
+
+
+def _is_overtrown_by_real_entry(exp_entry,
+                                entries,
+                                margin: dt.timedelta = dt.timedelta(days=7)):
+    for real_entry in entries:
+        if _have_same_postings_accounts([exp_entry, real_entry]):
+            if real_entry.date >= exp_entry.date - margin:
+                return True
+    return False
+    # pdb.set_trace()
 
 
 def expect(entries, options_map, config_string="{}"):
     ""
     errors = []
     forecasted = []
-    for entry in [entry for entry in entries if _is_origin(entry)]:
-        for expected_date in get_expected_dates(entry):
-            expected_entry = entry._replace(date=expected_date)
+    # process, from now on, only data.Transactions
+    txns = [e for e in entries if isinstance(e, data.Transaction)]
+    # iterate transactions that generate expectations
+    for txn in [txn for txn in txns if _is_origin(txn)]:
+        # get dates for expected transactions
+        expectations = get_expected_dates(txn)
+        # iterate the expected dates
+        for expected_date in expectations:
+            # copy the entry, using the expected date and tag with 'expected'
+            # TODO copy without medatdata
+            expected_entry = txn._replace(date=expected_date)
             expected_entry = tag_entry('expected', expected_entry)
-            # TODO compare with existing entries
-            # TODO if until is approaching, error that forecasted are finishing
-            forecasted.append(expected_entry)
-    # pdb.set_trace()
+            # check if the new entry is a duplicate of an existing one
+            # e.g., there is real transaction overwriting the expected
+            if _is_overtrown_by_real_entry(expected_entry, txns):
+                continue
+            else:
+                forecasted.append(expected_entry)
     return entries + forecasted, errors
+
+# TODO if until is approaching, error that forecasted are finishing
